1549e97ae77159f909be9e74156c910e1f21e179
joaogbr1411 <joaozaccara@gmail.com>
2025-04-28 23:14:25 -0300

    Update login_signup.py

--- login_signup.py
+++ login_signup.py
@@ -1,16 +1,17 @@
 from pathlib import Path
-import json
+import json ##importacao das bibliotecas
 
-path = Path('user.json')
+path = Path('user.json') ##definindo path do json
 
-
+##login com uma mensagem de boas vindas
 def login(data):
     if path.exists():
         info = json.loads(data)
         ver_login = input('Bem vindo de volta! Informe seu CPF: ')
         if ver_login == info['CPF']:
             print(f"Bem vindo de volta, {info['nome_completo']}")
-                            
+
+#cadastro e armazenamento de informações                            
 def cadastro():
     usuario = {
         'nome_completo': input('Digite seu nome completo: '),
@@ -24,11 +25,13 @@ def cadastro():
     print('Seu cadastro foi concluido')
     return data
 
+##verificação para cadastrar ou logar-se
+
 initial_ver = input('Você já possui uma conta? Digite SIM ou NAO: ')
-if initial_ver == 'NAO':
+if initial_ver == 'NAO': ##redireciona para criar uma conta
     cadastro()
 
-if initial_ver == 'SIM':    
+if initial_ver == 'SIM': ##captura as informações e redireciona para login    
     data = path.read_text()
     login(data)   
     
